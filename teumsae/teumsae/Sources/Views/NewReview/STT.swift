0aa31cfde6e8edc341f700993194cdc1816e870b
seungyeonlj <amyj97@snu.ac.kr>
2021-11-05 16:45:53 +0900

    add STT (incomplete)

---
+++ teumsae/teumsae/Sources/Views/NewReview/STT.swift
@@ -0,0 +1,117 @@
+//
+//  STT.swift
+//  teumsae
+//
+//  Created by seungyeon on 2021/11/05.
+//
+
+import Foundation
+import Alamofire
+
+func convertToText(){
+    let headers = ["Transfer-Encoding: chunked", "Content-Type: application/octet-stream", "Authorization: KakaoAK {REST_API_KEY}"]
+    //let recording = // TODO
+    AF.request("https://kakaoi-newtone-openapi.kakao.com/v1/recognize", method: .post, encoding: URLEncoding.httpBody).responseJSON() { response in
+      switch response.result {
+      case .success:
+        if let data = try! response.result.get() as? [String: Any] {
+          print(data)
+        }
+      case .failure(let error):
+        print("Error: \(error)")
+        return
+      }
+    }
+}
+
+/*
+import UIKit
+struct Response: Codable{
+    let success: Bool
+    let result: String
+    let message: String
+    
+}
+
+
+func requestGet(url: String, completionHandler: @escaping (Bool, Any) -> Void) {
+    guard let url = URL(string: url) else {
+        print("Error: cannot create URL")
+        return
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        guard error == nil else {
+            print("Error: error calling GET")
+            print(error!)
+            return
+        }
+        guard let data = data else {
+            print("Error: Did not receive data")
+            return
+        }
+        guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
+            print("Error: HTTP request failed")
+            return
+        }
+        guard let output = try? JSONDecoder().decode(Response.self, from: data) else {
+            print("Error: JSON Data Parsing failed")
+            return
+        }
+        
+        completionHandler(true, output.result)
+    }.resume()
+}
+
+func requestPost(url: String, method: String, param: [String: Any], completionHandler: @escaping (Bool, Any) -> Void) {
+    let sendData = try! JSONSerialization.data(withJSONObject: param, options: [])
+    
+    guard let url = URL(string: url) else {
+        print("Error: cannot create URL")
+        return
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = method
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpBody = sendData
+    
+    URLSession.shared.dataTask(with: request) { (data, response, error) in
+        guard error == nil else {
+            print("Error: error calling GET")
+            print(error!)
+            return
+        }
+        guard let data = data else {
+            print("Error: Did not receive data")
+            return
+        }
+        guard let response = response as? HTTPURLResponse, (200 ..< 300) ~= response.statusCode else {
+            print("Error: HTTP request failed")
+            return
+        }
+        guard let output = try? JSONDecoder().decode(Response.self, from: data) else {
+            print("Error: JSON Data Parsing failed")
+            return
+        }
+        
+        completionHandler(true, output.result)
+    }.resume()
+}
+
+func request(_ url: String, _ method: String, _ param: [String: Any]? = nil, completionHandler: @escaping (Bool, Any) -> Void) {
+    if method == "GET" {
+        requestGet(url: url) { (success, data) in
+            completionHandler(success, data)
+        }
+    }
+    else {
+        requestPost(url: url, method: method, param: param!) { (success, data) in
+            completionHandler(success, data)
+        }
+    }
+}
+ */

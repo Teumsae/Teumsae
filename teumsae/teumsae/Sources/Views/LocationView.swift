201d75995e5728808118b44fb0fdae83cddc3354
GoGiants1 <chooi221@snu.ac.kr>
2021-12-14 23:39:06 +0900

    init mapView with user location

--- teumsae/teumsae/Sources/Views/LocationView.swift
+++ teumsae/teumsae/Sources/Views/LocationView.swift
@@ -10,147 +10,45 @@
 import SwiftUI
 import MapKit
 
-/*
-struct City: Identifiable {
-    let id = UUID()
-    let coordinate: CLLocationCoordinate2D
-}
-
-struct LocationView: View {
-    @State private var cities: [City] = [
-            City(coordinate: .init(latitude: 40.7128, longitude: 74.0060)),
-            City(coordinate: .init(latitude: 37.7749, longitude: 122.4194)),
-            City(coordinate: .init(latitude: 47.6062, longitude: 122.3321))
-    ]
-
-    @State private var userTrackingMode: MapUserTrackingMode = .follow
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 37.532600,
-                                       longitude:     127.024612), // default: Seoul
-//        latitudinalMeters: 750,
-//        longitudinalMeters: 750
-        span: MKCoordinateSpan(
-                    latitudeDelta: 10,
-                    longitudeDelta: 10
-                )
-    )
-    
-    
-    func makeUIView(context: Self) -> MKMapView{
-        MKMapView(frame: .zero)
-
-    }
-    
-
-    func updateUIView(_ view: MKMapView, context: Self) {
-        let coordinate = CLLocationCoordinate2D(latitude: 37.532600,
-                                               longitude:     127.024612) // default: Seoul
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
-        let region = MKCoordinateRegion(center: coordinate, span: span)
-        view.setRegion(region, animated: true)
-
-    }
-
-    var body: some View {
-        Map(
-            coordinateRegion: $region,
-            interactionModes: MapInteractionModes.all,
-            showsUserLocation: true,
-            userTrackingMode: $userTrackingMode
-        )
-        Button("zoom") {
-            withAnimation {
-                region.span = MKCoordinateSpan(
-                    latitudeDelta: 100,
-                    longitudeDelta: 100
-                )
-            }
-        }
-//        Map(coordinateRegion: $region, annotationItems: cities) { city in
-//                    MapAnnotation(
-//                        coordinate: city.coordinate,
-//                        anchorPoint: CGPoint(x: 0.5, y: 0.5)
-//                    ) {
-//                        Circle()
-//                            .stroke(Color.green)
-//                            .frame(width: 44, height: 44)
-//                    }
-//                }
-    } //END OF BODY
-
-}
-
-struct LocationView_Previews: PreviewProvider {
-    static var previews: some View {
-        LocationView()
-    }
-}
- */
-
+// TODO: 위치 등록한거 DB에 저장하기. 기존에 등록된 알람 삭제 기능?
 
 
 
 
 struct LocationView: UIViewRepresentable {
-    let locationManager: LocationManager
-//    let entireMapViewCoordinator: EntireMapViewCoordinator
-    
-    init(locationManager: LocationManager){
-        self.locationManager = locationManager
-//        self.entireMapViewCoordinator = EntireMapViewCoordinator(self)
-    }
-        func updateUIView(_ mapView: MKMapView, context: Context) {
-            print("LocationView: updateUIView")
-            let span = MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3)
-            var coordinate = CLLocationCoordinate2D()
-            coordinate.latitude = 37.532600
-            coordinate.longitude = 127.024612  // default: Seoul
-            let region = MKCoordinateRegion(center: coordinate, span: span)
-            mapView.setRegion(region, animated: true)
-            
-            
-//            if (mapView.annotations.count > 0){
-//                print("LocationView: updateUIView: call updateCoreLocation")
-//                self.updateCoreLocation(mapView, context: context)
-//            }
+	let locationManager = LocationManager()
 
-        }
+	func updateUIView(_ mapView: MKMapView, context: Context) {
+		print("LocationView: updateUIView")
+		let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+		locationManager.locationManager.startUpdatingLocation()
+		// 유저의 현재 위치 받아와서 지도 시작
+		let latitude: Double = locationManager.locationManager.location?.coordinate.latitude ??  37.532600
+		let longitude: Double = locationManager.locationManager.location?.coordinate.longitude ??  127.024612
+		let coordinate = CLLocationCoordinate2DMake(latitude,longitude)
+		let region = MKCoordinateRegion(center: coordinate, span: span)
+		mapView.setRegion(region, animated: true)
+	}
     
      func makeUIView(context: Context) -> MKMapView {
-            print("LocationView: makeUIView")
-         let entireMapViewCoordinator: EntireMapViewCoordinator = EntireMapViewCoordinator(self, locationManager: locationManager)
+		print("LocationView: makeUIView")
+		let entireMapViewCoordinator: EntireMapViewCoordinator = EntireMapViewCoordinator(self, locationManager: locationManager)
          
-            let myMap = MKMapView(frame: .zero)
-            let longPress = UILongPressGestureRecognizer(target: context.coordinator, action: #selector(entireMapViewCoordinator.addAnnotation(gesture:)))
-            longPress.minimumPressDuration = 1
-            myMap.addGestureRecognizer(longPress)
-            myMap.delegate = context.coordinator
+		let myMap = MKMapView(frame: .zero)
+		let longPress = UILongPressGestureRecognizer(target: context.coordinator, action: #selector(entireMapViewCoordinator.addAnnotation(gesture:)))
+		longPress.minimumPressDuration = 1
+		myMap.addGestureRecognizer(longPress)
+		myMap.delegate = context.coordinator
+		myMap.showsUserLocation = true
+		print("LocationView: makeUIView: ", myMap.annotations.count)
             
-            myMap.showsUserLocation = true
-            print("LocationeView: makeUIView: ", myMap.annotations.count)
-            
-            return myMap
+		return myMap
         }
 
     func makeCoordinator() -> EntireMapViewCoordinator {
         return EntireMapViewCoordinator(self, locationManager: locationManager)
     }
-    
-//    func updateCoreLocation(_ mapView: MKMapView, context: Context) {
-//        let annotations = mapView.annotations
-//
-////        if (annotations.endIndex != 0){
-//            print("LocationView: coreLocation before update ", locationManager.location.latitude, locationManager.location.latitude)
-//
-//            let coreLocation = annotations[annotations.endIndex]
-//            locationManager.setCenterLocation(latitude: coreLocation.coordinate.latitude, longitude: coreLocation.coordinate.longitude)
-//            print("LocationView: coreLocation after update ", locationManager.location.latitude, locationManager.location.latitude)
-////        }
-////        else{
-////            print("LocationView: coreLocation no update ")
-////        }
-//    }
-    
+   
 
     class EntireMapViewCoordinator: NSObject, MKMapViewDelegate {
 
@@ -190,6 +88,7 @@ struct LocationView: UIViewRepresentable {
 
 struct LocationView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationView(locationManager: LocationManager())
+//        LocationView(locationManager: LocationManager())
+		LocationView()
     }
 }
